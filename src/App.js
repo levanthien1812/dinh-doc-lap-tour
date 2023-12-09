2933ec51b08b2f17d872baffbf81149b0715ece5
quocbao <20521097@gm.uit.edu.vn>
2023-12-09 16:16:25 +0700

    add header, footer page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import HomePage from './pages/home/HomePage';
 import SignupPage from './pages/signup/SignupPage';
 import LoginPage from './pages/login/LoginPage';
+import Introduction from './pages/Introduction/Introduction';
+import Model from './pages/Model/Model';
+import Review from './pages/Review/Review';
+import Booking from './pages/Booking/Booking';
 
 function App() {
   const router = createBrowserRouter([
@@ -21,9 +25,24 @@ function App() {
         {
           path: "/dang-nhap",
           element: <LoginPage/>
-        }
+        },
+        {
+          path: "/gioi-thieu",
+          element: <Introduction/>
+        },
+        {
+          path: "/mo-hinh",
+          element: <Model/>
+        },
+        {
+          path: "/danh-gia",
+          element: <Review/>
+        },
+        {
+          path: "/dat-ve",
+          element: <Booking/>
+        },
       ]
-      
     }
   ])
 
